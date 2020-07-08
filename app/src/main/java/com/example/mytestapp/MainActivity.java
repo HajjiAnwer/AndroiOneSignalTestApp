383217f621d32e77c586470c7a19de7cf3f73cb6
HajjiAnwer <hajjianwer2013@gmail.com>
2020-07-08 16:28:51 +0100

    fixing subscribe user to one signal nitification

--- app/src/main/java/com/example/mytestapp/MainActivity.java
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         // OneSignal Initialization
+        //OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
