990f7a3addbf7309b119f546ca67bc2e1b0e67c9
HajjiAnwer <hajjianwer2013@gmail.com>
2020-07-08 13:37:45 +0100

    first test

---
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mytestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.onesignal.OneSignal;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
